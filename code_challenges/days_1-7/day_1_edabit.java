8143a36c7281be2ec349829d2f9fd9f7835ba620
melonybox <grandmadcow@gmail.com>
2021-05-25 14:53:26 -0400

    Edabit day 1

---
+++ code_challenges/days_1-7/day_1_edabit.java
@@ -0,0 +1,103 @@
+public class Program {
+    public static boolean returnTrue() {
+      return true;
+    }
+}
+
+public class Challenge {
+	public static int convert(int minutes) {
+		return minutes * 60;
+  }
+}
+
+public class Program {
+	public static boolean isSameNum(int x, int y) {
+		return x == y;
+	}
+}
+
+public class Challenge {
+	public static int findPerimeter(int length, int width) {
+		return length*2 + width*2;
+  }
+}
+
+public class Program {
+    public static boolean lessThanOrEqualToZero(int num) {
+      return num <= 0;
+    }
+}
+
+public class Challenge {
+	public static boolean isEqual(int num1, int num2) {
+		return num1 == num2;
+  }
+}
+
+public class Challenge {
+  public static int remainder(int a, int b) {
+		return a % b;
+  }
+}
+
+public class Program {
+	public static String giveMeSomething(String a) {
+		return "something " + a;
+	}
+}
+
+public class Polygon {
+	public static int sumOfAngles(int n) {
+		return 180 * (n - 2);
+	}
+}
+
+public class Age {
+	public static int calcAge(int age) {
+		return age * 365;
+	}
+}
+
+public class Challenge {
+	public static int triArea(int base, int height) {
+		return (base * height) / 2;
+	}
+}
+
+public class Challenge {
+  public static boolean lessThan100(int a, int b) {
+		return a+b < 100;
+  }
+}
+
+public class SumOfTwoNumbers {
+    public static int sum(int a, int b) {
+      return a+b;
+    }
+}
+
+public class Program {
+  public static int footballPoints(int wins, int draws, int loses) {
+		return wins*3 + draws;
+  }
+}
+
+public class Challenge {
+	public static int convert(int hours, int minutes) {
+		int hoursMinutes = hours * 60;
+		return (hoursMinutes + minutes)*60;
+  }
+}
+
+public class Challenge {
+	public static int howManySeconds(int hours) {
+		return hours*60*60;
+  }
+}
+// return hours * 3600;
+
+public class Challenge {
+	public static int addition(int num) {
+		return num+1;
+	}
+}
