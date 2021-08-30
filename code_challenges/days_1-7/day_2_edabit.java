a4674a14813da0a75ce66eb5a163bea972e5d0a9
melonybox <grandmadcow@gmail.com>
2021-08-30 15:50:29 -0400

    Edabit addition

--- code_challenges/days_1-7/day_2_edabit.java
+++ code_challenges/days_1-7/day_2_edabit.java
@@ -25,3 +25,24 @@ public class Challenge {
 // 		return s.replace("i", "wi").replace("e", "we") + " owo";
 // 	}
 // }
+
+public class Vowels {
+  public static int getCount(String str) {
+    int vowelsCount = 0;
+    // Your code here
+		str = str.toLowerCase();
+    for (int i = 0; i < str.length(); ++i) {
+      char ch = str.charAt(i);
+      if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        ++vowelsCount;
+      }
+    }
+    return vowelsCount;
+  }
+}
+
+// public class Vowels {
+//   public static int getCount(String str) {
+//     return str.replaceAll("[^aeiouAEIOU]", "").length();
+//   }
+// }
